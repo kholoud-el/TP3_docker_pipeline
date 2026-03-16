8befb3867391e127d6a5e729046383fb10e589f7
kholoud-el <aoulakholoud@gmail.com>
2026-03-16 12:22:38 +0000

    ajouter et modifier des fichiers

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
-CMD ["python", "-m", "pytest", "tests/"]
+EXPOSE 5000
+CMD ["python", "./app/main.py"]
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -3,14 +3,15 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+from tasks import add_task, get_task
 
 def test_add_task():
     add_task("Learn Docker")
-    assert "Learn Docker" in get_tasks()
+    assert "Learn Docker" in get_task()
 
 def test_multiple_tasks():
     add_task("Learn CI")
     add_task("Learn DevOps")
-    tasks = get_tasks()
+    tasks = get_task()
     assert "Learn CI" in tasks
     assert "Learn DevOps" in tasks
